c3b803d10b6477273fad299fb4bf8f66d032fc62
Matthew-Mays <matthew.c.mays96@gmail.com>
2023-01-10 16:56:49 -0600

    hook up dummy data to home page and set up styles

---
+++ components/events/event-item.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import classes from "./event-item.module.css";
+
+const EventItem = (props) => {
+  const { title, image, date, location, id } = props;
+
+  const formatDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formatAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
+  return (
+    <li className={classes.item}>
+      <img src={"/" + image} alt="" />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{formatDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formatAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default EventItem;
